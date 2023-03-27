121bbd5af717e62d5f444602184436305dcb8f9a
Fhellipe Valentin <fhellipereis@outlook.com>
2023-03-27 14:56:14 -0300

    configuracao do h2 e adaptacao do id pra view

--- src/main/java/fhellipe/github/com/crudbyloianeback/model/Course.java
+++ src/main/java/fhellipe/github/com/crudbyloianeback/model/Course.java
@@ -1,5 +1,6 @@
 package fhellipe.github.com.crudbyloianeback.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,6 +16,7 @@ public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private Long id;
     private String name;
     private String category;
