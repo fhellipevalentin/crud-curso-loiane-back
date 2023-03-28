239f337cac291a2fbe868e365772c12f2f1231d3
Fhellipe Valentin <fhellipereis@outlook.com>
2023-03-28 14:49:28 -0300

    post para cursos

--- src/main/java/fhellipe/github/com/crudbyloianeback/controller/CourseController.java
+++ src/main/java/fhellipe/github/com/crudbyloianeback/controller/CourseController.java
@@ -5,14 +5,15 @@ import fhellipe.github.com.crudbyloianeback.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
+@CrossOrigin("http://localhost:4200/")
 public class CourseController {
 
     private final CourseRepository courseRepository;
@@ -24,6 +25,14 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping()
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        // System.out.println(course.getName());
+        return courseRepository.save(course);
+        // return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
     }
 
     @Bean
